9f54c434ab38ea4f1895d44c3f6da03c26dc804c
muyong <whatissrc@gmail.com>
2019-07-16 17:46:20 +0800

    fix sh

--- get-docker.sh
+++ get-docker.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 install-docker () {
 sudo apt update
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
 sudo apt update
 sudo apt install -y docker-ce
 }

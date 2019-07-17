85632521b838a4f5f18f470519d561f1235b6afb
muyong <whatissrc@gmail.com>
2019-07-17 09:41:59 +0800

    fix

--- get-docker.sh
+++ get-docker.sh
@@ -13,18 +13,22 @@ sudo apt-get -y install \
 # curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
 # sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce
 }
 
 install-compose () {
+  sudo apt update
   sudo apt install -y python3-pip
   sudo pip3 install docker-compose
 }
 
 last(){
 sudo usermod -a -G docker $USER
+docker-v
+docker-compose -v
 }
 
 install-docker
